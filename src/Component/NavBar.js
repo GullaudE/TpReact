350804c166bccd459ae8a74ffa624c47eb5790c7
Ewan Gullaud <ewan.gullaud@gmail.com>
2023-01-28 14:33:34 +0100

    NavBar + UserContext

---
+++ src/Component/NavBar.js
@@ -0,0 +1,89 @@
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {UserContext} from "./Context/UserContext";
+import React, {useContext} from "react";
+import {signOut} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
+import {auth} from "./Firebase"
+
+
+export default function NavBar(){
+
+    const {toggleModals} = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const logOut = async () => {
+        try {
+            await signOut(auth)
+            navigate("/")
+
+        }catch {
+            alert(" Déconnexion imposible")
+
+        }
+
+
+    }
+
+    return (
+        <div className="App">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <a href="/">
+                            <img
+                                src="/asset/image/RickMorty.png"
+                                width="60"
+                                height="60"
+                                alt="Rick&Morty"
+                            />
+                        </a>
+                    </Navbar.Brand>
+                    <Navbar.Brand>Rick & Morty</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+
+                        <Nav className="NavBar">
+                            <Nav.Link href="/Component/Episode">
+                                Episodes
+                            </Nav.Link>
+                            <Nav.Link href="/Component/Favoris">
+                                Favoris
+                            </Nav.Link>
+                            <div>
+                                <button
+
+                                    onClick={() => toggleModals("signUp")}
+                                    className="button-log">
+                                    Inscription
+                                </button>
+                                <button
+                                    onClick={() => toggleModals("signIn")}
+                                    className="button-log ms-2">
+                                    Connexion
+                                </button>
+                                <button
+                                    onClick={logOut}
+                                    className="button-log ms-2">
+                                    Déconnexion
+                                </button>
+                            </div>
+
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
+
+        </div>
+            )
+}
+
+
+
+
+
+
